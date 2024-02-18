0715bdaa682c0874e1c625f7fbd23a1bc6fc553b
roko-barbic <73283215+roko-barbic@users.noreply.github.com>
2024-02-19 00:27:38 +0100

    Add files via upload

---
+++ Igrica/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Igrica
+{
+    class Player
+    {
+        public int HealthPercentage { get; set; }
+        public int Health { get; set; }
+        public int CurrentHealth { get; set; }
+        public String Name { get; set; }
+        public Player()
+        {
+            Name = "noob";
+            Health = 1;
+            CurrentHealth = Health;
+        }
+
+        public Player(int health, string name)
+        {
+            Health = health;
+            Name = name;
+            CurrentHealth = health;
+        }
+        public void Attack(Player enemyPlayer)
+        {
+            Random random = new Random();
+            int attackDmg = random.Next() % 100;
+            enemyPlayer.CurrentHealth -= attackDmg;
+            enemyPlayer.CalculatePerc();
+            if (enemyPlayer.CurrentHealth <= 0)
+                enemyPlayer.CurrentHealth = 0;
+            Console.WriteLine(Name + " attacks " + enemyPlayer.Name + " => " + attackDmg + " damage");
+        }
+        public void CalculatePerc()
+        {
+            HealthPercentage = (int)(CurrentHealth / Health) * 100;
+        }
+
+    }
+}
---
+++ Igrica/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Igrica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(1000, "Player One");
+            Player player2 = new Player(599, "Player Two");
+            do
+            {
+                player1.Attack(player2);
+                if(player2.CurrentHealth > 0)
+                {
+                    player2.Attack(player1);
+                }
+            } while (player1.CurrentHealth > 0 && player2.CurrentHealth > 0);
+            Console.WriteLine(player1.Name + " " + player1.CurrentHealth + " vs " + player2.Name + " " + player2.CurrentHealth);
+            Console.WriteLine(player1.CurrentHealth == 0 ? player1.Name : player2.Name + " you Lose! ");
+            Console.ReadKey();
+        }
+        
+    }
+}
